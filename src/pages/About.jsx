bddf86c3efb2b99240a58f9f7dad0be440536c91
Muneeswaran <muneeswaranmd2004@gmail.com>
2026-05-11 18:11:12 +0530

    Remove team section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import WhyChooseUs from '../components/WhyChooseUs';
-import TeamSection from '../components/TeamSection';
 import CTASection from '../components/CTASection';
 
 const About = () => {
@@ -9,7 +8,6 @@ const About = () => {
     <div className="overflow-x-hidden pt-20">
       <AboutSection />
       <WhyChooseUs />
-      <TeamSection />
       <CTASection />
     </div>
   );
